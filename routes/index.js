67a572b2d04725cda13a7af29d98e2e9ed5ace50
Wil1i <williams.ca001@gmail.com>
2023-08-22 13:43:11 +0330

    Routes completed

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,22 @@ Router.get("/login", loginController.get);
 const registerController = require("../controllers/registerController");
 Router.get("/register", registerController.get);
 
+const shopController = require("../controllers/shopController");
+Router.get("/shop", shopController.get);
+
+const productController = require("../controllers/productController");
+Router.get("/product/:id", productController.get);
+
+const blogsController = require("../controllers/blogsController");
+Router.get("/blogs", blogsController.get);
+
+const singleBlogController = require("../controllers/singleBlogController");
+Router.get("/blog/:id", singleBlogController.get);
+
+const aboutUsController = require("../controllers/aboutUsController");
+Router.get("/about-us", aboutUsController.get);
+
+const rulesController = require("../controllers/rulesController");
+Router.get("/rules", rulesController.get);
+
 module.exports = Router;
